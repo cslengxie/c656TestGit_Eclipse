6463132a703296f3c375799e4b103818785d6be3
eclipse_user <cslengxie@163.com>
2020-10-20 13:41:47 +0800

    First Commit In Eclipse

---
+++ src/main/java/com/crx/git/Apple.java
@@ -0,0 +1,9 @@
+package com.crx.git;
+
+public class Apple {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+	
+}
---
+++ src/test/java/com/crx/test/GoodTest.java
@@ -0,0 +1,9 @@
+package com.crx.test;
+
+public class GoodTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello GoodTest");
+	}
+	
+}
